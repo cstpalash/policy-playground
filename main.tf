5f5f6496146847fca74c3fddbb8867dd050dc95b
Palash Roy <palashroy@Palashs-MacBook-Pro.local>
2021-08-13 13:20:05 +0530

    s3 test

---
+++ main.tf
@@ -0,0 +1,15 @@
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "storage-test-dev"
+  acl    = "private"
+
+  versioning = {
+    enabled = true
+  }
+
+  tags = {
+    env = "dev"
+  }
+
+}
