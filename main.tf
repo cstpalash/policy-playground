a74be9a0b0eb50e79c976091287cb9e98eff8889
Palash Roy <palashroy@Palashs-MacBook-Pro.local>
2021-08-13 15:13:20 +0530

    s3 test

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = vars.region
+}
+
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
