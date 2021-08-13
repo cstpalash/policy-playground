2af91751b35ac5c89b7c81867279af14887565f1
Palash Roy <palashroy@Palashs-MacBook-Pro.local>
2021-08-13 13:56:11 +0530

    s3 test

--- main.tf
+++ main.tf
@@ -1,8 +1,15 @@
+provider "aws" {
+  profile = "default"
+  region  = var.aws_region
+}
+
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
   bucket = "storage-test-dev"
   acl    = "private"
+
+  region = vars.region
 
   versioning = {
     enabled = true
