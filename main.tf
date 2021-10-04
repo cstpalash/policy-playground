59f9c8af40ab4ef64ec09595c40fd1ff202fd431
cstpalash <cstpalash@gmail.com>
2021-10-04 15:38:18 +0530

    kms encryption

--- main.tf
+++ main.tf
@@ -18,6 +18,15 @@ resource "aws_s3_bucket" "demo" {
   bucket = "${var.s3_bucket_name}"
   acl    = "private"
 
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "arn:aws:kms:us-west-2:611511327901:key/a98051f9-3213-4126-b438-111122246bef"
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+
   tags = {
     Environment = "Dev"
   }
@@ -27,4 +36,12 @@ resource "aws_s3_bucket" "demo1" {
   bucket = "another-test-bucket-8445"
   acl    = "private"
   
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "arn:aws:kms:us-west-2:611511327901:key/a98051f9-3213-4126-b438-111122246bef"
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
 }
