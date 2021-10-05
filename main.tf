ec38b373f27a7f64a64eb1ffd78d70c88e4ae0dd
cstpalash <cstpalash@gmail.com>
2021-10-05 13:12:15 +0530

    kms encryption

--- main.tf
+++ main.tf
@@ -18,14 +18,6 @@ resource "aws_s3_bucket" "demo" {
   bucket = "${var.s3_bucket_name}"
   acl    = "private"
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = "arn:aws:kms:us-west-2:611511327901:key/a98051f9-3213-4126-b438-111122246bef"
-        sse_algorithm     = "aws:kms"
-      }
-    }
-  }
 
   tags = {
     Environment = "Dev"
